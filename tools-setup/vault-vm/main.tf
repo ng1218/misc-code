35db4c0b5c470d54d9082ab9b6b1b726e42fbd2a
i340771 <naresh.ganta@sap.com>
2025-06-12 09:26:18 +0530

    test

--- tools-setup/vault-vm/main.tf
+++ tools-setup/vault-vm/main.tf
@@ -15,58 +15,58 @@ resource "azurerm_public_ip" public_ip {
   name                    = var.name
   location                = var.location
   resource_group_name     = var.resource_group_name
-  allocation_method       = "Static"
+  allocation_method       = "Dynamic"
 }
 
 ###### MOVED TO SPOT VM CREATION ###############
-resource "azurerm_virtual_machine" "vm" {
-  name                  = var.name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.privateip.id]
-  vm_size               = var.vm_size
-
-  delete_os_disk_on_termination = true
-
-  storage_image_reference {
-   id = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
-  }
-  storage_os_disk {
-    name              = "${var.name}-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = var.name
-    admin_username = "azuser"
-    admin_password = "devops@123456"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
-
-
-# resource "azurerm_linux_virtual_machine" "vm" {
+# resource "azurerm_virtual_machine" "vm" {
 #   name                  = var.name
 #   location              = var.location
 #   resource_group_name   = var.resource_group_name
-#   size                  = var.vm_size
-#   admin_username        = "adminuser"
-#   admin_password        = "devops@123456" 
 #   network_interface_ids = [azurerm_network_interface.privateip.id]
+#   vm_size               = var.vm_size
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
+#   delete_os_disk_on_termination = true
+
+#   storage_image_reference {
+#    id = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
 #   }
-
-#   source_image_id           = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
-#   priority                  = "Spot"
-#   eviction_policy           = "Deallocate"
-#   max_bid_price             = -1 
+#   storage_os_disk {
+#     name              = "${var.name}-disk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = var.name
+#     admin_username = "azuser"
+#     admin_password = "devops@123456"
+#   }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
 # }
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = var.name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  admin_password        = "devops@123456" 
+  network_interface_ids = [azurerm_network_interface.privateip.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id           = "/subscriptions/1c8859e3-276f-40f1-afc9-1dd8f8dc18d7/resourceGroups/myfirstvm_group/providers/Microsoft.Compute/images/local-devops-practice"
+  priority                  = "Spot"
+  eviction_policy           = "Deallocate"
+  max_bid_price             = -1 
+}
 
 
 

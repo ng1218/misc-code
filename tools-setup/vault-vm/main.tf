a8609658750d4cbda82e4965a2dcd38e8a2dd60e
i340771 <naresh.ganta@sap.com>
2025-06-12 09:30:36 +0530

    test

--- tools-setup/vault-vm/main.tf
+++ tools-setup/vault-vm/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_public_ip" public_ip {
   location                = var.location
   resource_group_name     = var.resource_group_name
   allocation_method       = "Dynamic"
+  sku                     = "Basic" 
 }
 
 ###### MOVED TO SPOT VM CREATION ###############

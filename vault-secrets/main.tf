11ee81aae372523c4073a9c3b33778e9c6c396d1
i340771 <naresh.ganta@sap.com>
2025-06-14 22:03:48 +0530

    test

--- vault-secrets/main.tf
+++ vault-secrets/main.tf
@@ -9,8 +9,8 @@ resource "vault_mount" "secrets" {
 resource "vault_kv_secret_v2" "secretvalues" {
     depends_on          = [ vault_mount.secrets ]
     for_each            = var.secret_values 
-    mount               = each.key
-    name                = each.value["secret_engine"]
+    mount               = each.value["secret_engine"]
+    name                = each.key
     delete_all_versions = false
     data_json           = jsonencode(each.value["secret_values"])
 }

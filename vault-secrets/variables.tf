53a435a1a15e74c3a36464404d6c6d0be8eacc7b
i340771 <naresh.ganta@sap.com>
2025-06-14 21:21:43 +0530

    test

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -9,5 +9,5 @@ variable "secret_engines" {
 variable "token" {}
 
 variable "vault_address" {
-  default  = "https://vault.nareshdevops1218.online:8200"
+  default  = "http://vault.nareshdevops1218.online:8200"
 }

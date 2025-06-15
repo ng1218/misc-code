47f551ccfbec4e049aa28e14e87087461ba7d480
i340771 <naresh.ganta@sap.com>
2025-06-15 09:50:29 +0530

    test

--- vault-secrets/variables.tf
+++ vault-secrets/variables.tf
@@ -30,5 +30,15 @@ variable "secret_values" {
               password = "devops@123456"
           }
         }
+        frontend = {
+          secret_engine = "roboshop-dev"
+          secret_values = {
+              catalogue_url = "http://catalogue-dev.nareshdevops1218.online:8080/"
+              user_url = "http://user-dev.nareshdevops1218.online:8080/"
+              cart_url = "http://cart-dev.nareshdevops1218.online:8080/"
+              shipping_url = "http://shipping-dev.nareshdevops1218.online:8080/"
+              payment_url = "http://payment-dev.nareshdevops1218.online:8080/"
+          }
+        }
     }
 }

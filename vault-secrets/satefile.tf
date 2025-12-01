a9a2af86e4467cf9569678f2ab7d8a7252cd202f
i340771 <naresh.ganta@sap.com>
2025-12-01 11:57:46 +0530

    updating code

--- vault-secrets/satefile.tf
+++ vault-secrets/satefile.tf
@@ -7,9 +7,9 @@ terraform {
   backend "azurerm" {
     use_cli              = true                                    # Can also be set via `ARM_USE_MSI` environment variable.
     #tenant_id            = "00000000-0000-0000-0000-000000000000"  # Can also be set via `ARM_TENANT_ID` environment variable.
-    subscription_id      = "1c8859e3-276f-40f1-afc9-1dd8f8dc18d7"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    resource_group_name  = "myfirstvm_group"
-    storage_account_name = "nareshdevops"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    subscription_id      = "ddffee8a-e239-4aa1-b7e0-b88ff5a2f9aa"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
+    resource_group_name  = "ngresources"
+    storage_account_name = "nareshdevops1"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "roboshop-state-files"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "vault.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }

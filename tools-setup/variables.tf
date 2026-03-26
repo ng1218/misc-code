1fbc0473392c9b77d493b39cd836279b0962e376
i340771 <naresh.ganta@sap.com>
2026-03-26 13:00:22 +0530

    updating code

--- tools-setup/variables.tf
+++ tools-setup/variables.tf
@@ -16,15 +16,19 @@ variable "tools" {
             vm_size = "Standard_B2als_v2"
             spot = true
         }
-        github-runner = {
+        /*github-runner = {
           vm_size = "Standard_D2ls_v5"
           spot = false
-        }
+        }*/
         /*elk-stack = {
           vm_size = "Standard_E2bds_v5"
           spot = false
         }*/
         sonarqube = {
+          vm_size = "Standard_D2ls_v5"
+          spot = false
+        }
+        artifactory = {
           vm_size = "Standard_D2ls_v5"
           spot = false
         }
